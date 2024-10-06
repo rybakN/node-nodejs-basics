8fbfcc0d21053331b65f05ca7b812e0f1eb96c88
rybakN <dnb4949@gmail.com>
2024-10-06 21:56:44 +0300

    feat: part Command line interface completed

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import { argv } from "node:process";
+import { paintText } from "../common/helpers.js";
+
 const parseArgs = () => {
-    // Write your code here 
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].startsWith("--")) {
+      console.log(paintText(`${argv[i]} is ${argv[i + 1]}`, "green"));
+    }
+  }
 };
 
 parseArgs();
