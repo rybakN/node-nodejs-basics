cbfe76dfcfb9102e925312112803cacf1d74eec5
rybakN <dnb4949@gmail.com>
2024-10-07 12:42:25 +0300

    feat: part Worker Threads completed

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { cpus } from "node:os";
+import { join } from "node:path";
+import { Worker } from "node:worker_threads";
+import { getDirname } from "../common/helpers.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+  const pathToWorker = join(__dirname, "worker.js");
+  const coreCount = cpus().length;
+  const workers = [];
+  for (let i = 0; i < coreCount; i++) {
+    workers.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(pathToWorker);
+        worker.postMessage({ n: 10 + i });
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", (code) => {
+          if (code !== 0) {
+            reject();
+          }
+        });
+      }),
+    );
+  }
+  const result = await Promise.allSettled(workers);
+  console.log(
+    result.map((worker) => {
+      return worker.status === "fulfilled"
+        ? { status: "resolved", data: worker.value }
+        : { status: "error", data: null };
+    }),
+  );
 };
 
 await performCalculations();
