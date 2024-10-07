0ab043bfe7664894fda575baecedc3cc721421f2
rybakN <dnb4949@gmail.com>
2024-10-07 08:01:50 +0300

    feat: part Hash completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { join } from "path";
+import { getDirname } from "../common/helpers.js";
+import { createReadStream } from "node:fs";
+
+const { createHash } = await import("node:crypto");
+const __dirname = getDirname(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const src = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const hash = createHash("sha256");
+
+  const input = createReadStream(src);
+
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")} ${src}`);
+    }
+  });
 };
 
 await calculateHash();
