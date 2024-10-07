bae32aa4a61f6681c8c90da60aa51cb9ecb76d31
rybakN <dnb4949@gmail.com>
2024-10-07 10:43:23 +0300

    feat: part Zlib completed

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import fs from "node:fs";
+import { join } from "node:path";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { getDirname, paintText } from "../common/helpers.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+  const srcRead = join(__dirname, "files", "archive.gz");
+  const srcWrite = join(__dirname, "files", "fileToCompress.txt");
+
+  const readStream = fs.createReadStream(srcRead);
+  const writeStream = fs.createWriteStream(srcWrite);
+
+  try {
+    await pipeline(readStream, createUnzip(), writeStream);
+  } catch (err) {
+    paintText(err.message);
+  }
 };
 
 await decompress();
